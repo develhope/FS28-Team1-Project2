aa66575ee37795f62debd0401cfcf27b7709be67
Adamo Fall <adamofall23@gmail.com>
2024-05-17 12:22:29 +0200

    Function complete task

--- TaskMs.js
+++ TaskMs.js
@@ -1,5 +1,5 @@
 //1- Initialize an empty array named tasks to store task names.
-const task = [];
+const task = ["Task 1", "Task 2", "Task 3", "Task 4"];
 
 //2-* Function to Add a Task:
 // * Define a function named addTask that takes two parameters:
@@ -16,12 +16,33 @@ const task = [];
 // ### Define a function named completeTask that takes two parameters:
 // * index: An integer representing the index of the task to complete.
 // * callback: A callback function to handle the result.(FRANK)
-
 // Inside the completeTask function, use setTimeout to simulate an asynchronous operation.
 // If the index parameter is out of range (less than 0 or greater than or equal to the length of the tasks array),
 // invoke the callback function with an Error object containing the message "Invalid task index".
 // Otherwise, remove the task at the specified index from the tasks array and invoke the callback function with null
 // as the first parameter and the message "Task completed successfully" as the second parameter.(ADAMO)
+
+function completeTask(index, callback) {
+  setTimeout(() => {
+    if (index < 0 || index >= task.length) {
+      callback(new Error("Invalid task index"));
+    } else {
+      task.splice(index, 1);
+      callback(null, "Task completed successfully");
+    }
+  }, 1000);
+};
+
+function handleResult(error, message) {
+  if (error) {
+    console.error(error.message);
+  } else {
+    console.log(message);
+  }
+};
+
+completeTask(1, handleResult);
+completeTask(10, handleResult);
 
 //4-Function to List Tasks:
 // Define a function named listTasks that does not take any parameters.
